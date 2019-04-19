514a3c782c38b50e9939ea320e3f9c878b76f5f9
Beto Dute <betodute@gmail.com>
2019-04-19 04:01:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(a){
+  var location = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (var i=0; i<location.length; i++) {
+    location[i].innerHTML=parseInt(location[i].innerHTML)+a; 
+  }
+}
+
+function deepestChild(){
+  var location2 = document.getElementById("grand-node").querySelectorAll("div");
+  var storage;
+  for (var i=0; i<location2.length-1; i++) {
+    storage = location2[i].querySelector("div");
+  }
+  return storage;
+}
